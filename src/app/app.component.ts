d46c4b00c6ca16a7330bf7f0f8c0b499180f5f98
Shelby <tateremail@gmail.com>
2021-01-18 21:38:10 -0600

    updated posts service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -12,50 +13,32 @@ export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
   }
 
   onCreatePost(postData: Post) {
-    // Send Http request
-    this.http.post<{ name: string }>( //recommended to let it know what type of data it is
-      'https://ng-complete-guide-e532a-default-rtdb.firebaseio.com/posts.json',
-      postData
-      ).subscribe(responseData => {
-        console.log(responseData); 
-      });
+    this.postsService.createAndStorePost(postData.title, postData.content);
+
   }
 
   onFetchPosts() {
-    // Send Http request
-    this.fetchPosts();
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
   }
 
   onClearPosts() {
     // Send Http request
   }
 
-  private fetchPosts() {
-    this.isFetching = true;
-    this.http
-      .get<{ [key: string]: Post }>('https://ng-complete-guide-e532a-default-rtdb.firebaseio.com/posts.json')//ng now understands responseData will have this format
-      .pipe(
-        map(responseData => {
-          const postsArray: Post[] = [];
-          for (const key in responseData) {
-            if (responseData.hasOwnProperty(key)) {
-            postsArray.push({ ...responseData[key], id: key })
-            } //end if
-          } //end for
-          return postsArray;
-        }) //end map
-      ) //end pipe
-      .subscribe(posts => {
-        this.isFetching =  false; 
-        this.loadedPosts = posts;
-      });
-  }
 
 }
---
+++ src/app/posts.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Post } from "./post.model";
+import { map } from 'rxjs/operators'
+
+@Injectable({providedIn: 'root'})
+
+export class PostsService {
+
+    constructor(private http: HttpClient) {}
+
+    createAndStorePost(title: string, content: string) {
+        const postData: Post = {title: title, content: content}
+        this.http.post<{ name: string }>( //recommended to let it know what type of data it is
+            'https://ng-complete-guide-e532a-default-rtdb.firebaseio.com/posts.json',
+            postData
+            ).subscribe(responseData => {
+              console.log(responseData); 
+            });
+    }
+}
+
+fetchPosts() {
+    return this.http
+      .get<{ [key: string]: Post }>('https://ng-complete-guide-e532a-default-rtdb.firebaseio.com/posts.json')//ng now understands responseData will have this format
+      .pipe(
+        map(responseData => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+            postsArray.push({ ...responseData[key], id: key })
+            } //end if
+          } //end for
+          return postsArray;
+        }) //end map
+      ); //end pipe 
+}
