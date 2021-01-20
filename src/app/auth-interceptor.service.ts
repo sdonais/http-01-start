98caf97d958addf183920e3d65894e1888c35b38
Shelby <tateremail@gmail.com>
2021-01-20 00:58:32 -0600

    logged interceptor response

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,9 +1,20 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { tap } from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         console.log('Request sent');
-        return next.handle(req); 
+        console.log(req.url);
+        const modifiedRequest = req.clone({
+            headers: req.headers.append('Auth', 'made-up')
+        });
+        return next.handle(modifiedRequest).pipe(tap(event => {
+            console.log(event);
+            if (event.type === HttpEventType.Response) {
+                console.log('Response arrived; body data: ');
+                console.log(event.body);
+            }
+        })); 
     }
 
 
