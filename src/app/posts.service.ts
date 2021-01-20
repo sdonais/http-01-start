80f8ab781cead46909e4082af33b8b61699b7aad
Shelby <tateremail@gmail.com>
2021-01-20 00:05:14 -0600

    added query params

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { map, catchError } from 'rxjs/operators'
@@ -24,11 +24,16 @@ export class PostsService {
     }
 
     fetchPosts() {
+        let searchParams = new HttpParams();
+        searchParams = searchParams.append('print', 'pretty');
+
         return this.http
         .get<{ [key: string]: Post }>(
             'https://ng-complete-guide-e532a-default-rtdb.firebaseio.com/posts.json',
             {
-                headers: new HttpHeaders({'Custom-Header': 'hello'})
+                headers: new HttpHeaders({'Custom-Header': 'hello'}),
+                params: new HttpParams().set('print', 'pretty'),
+                //params: searchParams
             })
         .pipe(
             map(responseData => {
