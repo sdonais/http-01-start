963092926c10f26948b5f543d4a96c9239020dcc
Shelby <tateremail@gmail.com>
2021-01-19 23:43:46 -0600

    implemented catchError

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
-import { map } from 'rxjs/operators'
-import { Subject } from "rxjs";
+import { map, catchError } from 'rxjs/operators'
+import { Subject, throwError } from "rxjs";
 
 @Injectable({providedIn: 'root'})
 
@@ -36,7 +36,11 @@ export class PostsService {
                 } //end if
             } //end for
             return postsArray;
-        }) //end map
+        }), //end map
+        catchError(errorRes => {
+            // generic error handling task, like send to analytics server
+            return throwError(errorRes);
+        })
       ); //end pipe 
     } //end fetchPosts
 
